f23840cac4720529ab077d0094dd7e7216806167
dario <dariodepaulamaciel@hotmail.com>
2022-11-16 16:49:49 -0300

    Usando Slidable

--- lib/widgets/todo_list_Item.dart
+++ lib/widgets/todo_list_Item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:listadetarefas/models/todo.dart';
 
